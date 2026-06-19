aef7a5bd7d3a9c9a007e9729719add7249aea387
dbwoaud <ryujm3410@naver.com>
2026-06-19 23:12:32 +0900

    플레이어 이동 기능 수정

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
 using UnityEngine;
+
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
+    public enum InputAction { Up, Down, Left, Right, Grab, Interact, Run }
+
     [Header("키 바인딩")]
     [SerializeField] private KeyCode upKey = KeyCode.W;
     [SerializeField] private KeyCode downKey = KeyCode.S;
@@ -13,45 +17,43 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private KeyCode runKey = KeyCode.LeftAlt;
 
     [Header("이동 설정")]
-    [SerializeField] private float moveSpeed = 10f; 
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float runMultiplier = 2.0f;
     [SerializeField] private float rotationSpeed = 15f;
     [SerializeField] private float gravity = -9.8f;
 
     private CharacterController controller;
-    private float verticalVelocity;  // 수직 속도(중력 누적용)
+    private float verticalVelocity;
+    private Dictionary<InputAction, KeyCode> binds;
 
     void Awake()
     {
         controller = GetComponent<CharacterController>();
+        binds = new Dictionary<InputAction, KeyCode>
+        {
+            { InputAction.Up,       upKey },
+            { InputAction.Down,     downKey },
+            { InputAction.Left,     leftKey },
+            { InputAction.Right,    rightKey },
+            { InputAction.Grab,     grabKey },
+            { InputAction.Interact, interactionKey },
+            { InputAction.Run,      runKey },
+        };
     }
+
+    private bool IsHeld(InputAction a) => Input.GetKey(binds[a]);
+    private bool WasPressed(InputAction a) => Input.GetKeyDown(binds[a]);
 
     void Update()
     {
-        float x = 0f;
-        float z = 0f;
+        HandleMovement();
+        HandleActions();
+    }
 
-        if (Input.GetKey(upKey))
-            z += 1f;
-        if (Input.GetKey(downKey))
-            z -= 1f;
-        if (Input.GetKey(leftKey))
-            x -= 1f;
-        if (Input.GetKey(rightKey))
-            x += 1f;
-
-        if (Input.GetKeyDown(runKey))
-            moveSpeed = 20f;
-
-        if (Input.GetKey(grabKey))
-        {
-            Debug.Log("그랩 키 누름");
-        }
-
-        if(Input.GetKeyDown(interactionKey))
-        {
-            Debug.Log("상호작용 키 누름");
-        }
-
+    private void HandleMovement()
+    {
+        float x = (IsHeld(InputAction.Right) ? 1f : 0f) - (IsHeld(InputAction.Left) ? 1f : 0f);
+        float z = (IsHeld(InputAction.Up) ? 1f : 0f) - (IsHeld(InputAction.Down) ? 1f : 0f);
         Vector3 moveInput = new Vector3(x, 0f, z).normalized;
 
         if (controller.isGrounded && verticalVelocity < 0f)
@@ -59,18 +61,27 @@ public class PlayerMovement : MonoBehaviour
         else
             verticalVelocity += gravity * Time.deltaTime;
 
-        Vector3 velocity = moveInput * moveSpeed;
+        float speed = IsHeld(InputAction.Run) ? moveSpeed * runMultiplier : moveSpeed;
+
+        Vector3 velocity = moveInput * speed;
         velocity.y = verticalVelocity;
         controller.Move(velocity * Time.deltaTime);
-
         if (moveInput.sqrMagnitude > 0.01f)
         {
-            Quaternion targetRotation = Quaternion.LookRotation(moveInput, Vector3.up);
-            transform.rotation = Quaternion.Slerp(
-                transform.rotation,
-                targetRotation,
-                rotationSpeed * Time.deltaTime
-            );
+            Quaternion target = Quaternion.LookRotation(moveInput, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, target, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void HandleActions()
+    {
+        if (WasPressed(InputAction.Grab))
+        {
+            Debug.Log("잡기 기능");
+        }
+        if (WasPressed(InputAction.Interact))
+        {
+            Debug.Log("상호작용 기능");
         }
     }
 }
