ef508bec657c391726c2737a0137f40d187dbd70
NFUE2 <96811655+NFUE2@users.noreply.github.com>
2026-06-19 23:38:06 +0900

    Add : Singleton Script

---
+++ Assets/Scripts/Common/Singleton.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    [SerializeField] private bool isDestroyable; //파괴 가능여부
+
+    private static T m_instance;
+
+    public static T Instance 
+    { 
+        get
+        {
+            if(m_instance == null)
+            {
+                
+                if(m_instance == null)
+                {
+                    var g = new GameObject(typeof(T).Name);
+                    m_instance = g.AddComponent<T>();
+                }
+            }            
+            
+            return m_instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if(m_instance == null)
+        {
+            m_instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        if(isDestroyable == false)
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
